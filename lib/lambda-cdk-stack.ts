c47b253aa0fcae2ac1ca2a13a678f5fd3cfdb26a
Kingsley Ijomah <git-example@codehance>
2022-10-12 17:46:46 -0400

    Add lambda rest api

--- lib/lambda-cdk-stack.ts
+++ lib/lambda-cdk-stack.ts
@@ -1,6 +1,7 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 import * as lambda from 'aws-cdk-lib/aws-lambda';
+import { LambdaRestApi } from 'aws-cdk-lib/aws-apigateway';
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
 
 export class LambdaCdkStack extends cdk.Stack {
@@ -12,6 +13,11 @@ export class LambdaCdkStack extends cdk.Stack {
       runtime: lambda.Runtime.NODEJS_14_X,
       code: lambda.Code.fromAsset('lambda'),
       handler: 'hello.handler',
+    });
+
+    // create LambdaRestApi
+    new LambdaRestApi(this, 'Endpoint', {
+      handler: hello,
     });
   }
 }
